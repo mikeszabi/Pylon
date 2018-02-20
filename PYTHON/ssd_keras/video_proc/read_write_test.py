7219808d4675ca82811778f81c9f402280777f63
mikeszabi <szabolcs.mike@gmail.com>
2018-02-20 10:02:59 +0100

    VM pass

--- PYTHON/ssd_keras/video_proc/read_write_test.py
+++ PYTHON/ssd_keras/video_proc/read_write_test.py
@@ -9,6 +9,8 @@ import numpy as np
 import cv2
 import os
 print(cv2.__version__)
+import object_roi_detector as oroid
+
 
 import time
 img_channels = 3 # Number of color channels of the input images
@@ -17,6 +19,20 @@ img_width = 272 # Width of the input images
 new_height=img_height
 
 crop_mode='left' # left, right, middle
+
+"""
+init
+"""
+model_file=r'./models/ssd7_pylon.h5'
+roid = oroid.ssd_detection(model_file=model_file)
+
+imp=oroid.image_prepare(new_height = img_height, dx_roi_pct=25, crop_mode=crop_mode)
+    
+classes = ['background',
+           'concretepylon', 'metalpylon', 'woodpylon']
+
+merge_dict = {'concretepylon':'pylon','metalpylon':'pylon','woodpylon':'pylon'}
+merged_classes=['background','pylon']
 
 """
 read
@@ -59,31 +75,12 @@ while(cap.isOpened()):
     if ret==True:
         frame=cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 
-        scale = new_height / frame.shape[0]
-        dim = (int(frame.shape[1] * scale),new_height)
-         
-        # perform the actual resizing of the image and show it
-        im_resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+        imp.im=frame.copy()
+        im_square=imp.resize_square_image()
+
+        roi_box=roid.detect_roi(im_square)
         
-        # select one color channel
-        if img_channels==1:
-            im_resized=np.expand_dims(im_resized[:,:,2], axis=2)
-        
-        im_square=np.zeros((1,img_height,img_width,img_channels),'uint8')
-        
-        image_aspect_ratio = dim[0] / dim[1]
-        
-        
-        if image_aspect_ratio < 1:
-            im_square[0,:,int(img_width/2-dim[0]/2):int(img_width/2+dim[0]/2),:]=im_resized
-        else:
-            if crop_mode=='middle':
-                im_square[0,:,:,:]=im_resized[:,int(dim[0]/2-img_width/2):int(dim[0]/2+img_width/2),:]
-            elif crop_mode=='left':
-                im_square[0,:,:,:]=im_resized[:,0:img_width,:]
-            elif crop_mode=='right':
-                im_square[0,:,:,:]=im_resized[:,dim[0]-img_width:dim[0],:]
-           
+      
 #        cv2.imshow('frame',im_square[0,:,:,:])
 #        cv2.waitKey(0)
         """
@@ -91,24 +88,16 @@ while(cap.isOpened()):
         """
         
         t=time.time()
-        y_pred = model.predict(im_square)
-        # 4: Decode the raw prediction `y_pred`
-        y_pred_decoded = decode_y2(y_pred,
-                                   confidence_thresh=0.01,
-                                   iou_threshold=0.25,
-                                   top_k='all',
-                                   input_coords='centroids',
-                                   normalize_coords=False,
-                                   img_height=img_height,
-                                   img_width=img_width)
+        roi_box=roid.detect_roi(im_square)
+
         
         print(time.time()-t)
         
-        frame_out=im_square[0,:,:,:].copy()
+        frame_out=im_square.copy()
         if img_channels==1:
             frame_out=cv2.cvtColor(frame_out,cv2.COLOR_GRAY2RGB)
             
-        for box in y_pred_decoded[0]:
+        for box in roi_box[0]:
             print('yes')
             label = '{}: {:.2f}'.format(merged_classes[int(box[0])], box[1])
             startX=int(box[2])
